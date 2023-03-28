6b811c10702833e125fea8d38fe4073944e36483
AhBiel <95438103+AngeloGbiel@users.noreply.github.com>
2023-03-27 23:07:16 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Buttons from '../components/calculator/Buttons'
 import Delete from '../components/calculator/Delete'
@@ -17,11 +17,43 @@ const Container = styled.div`
 `
 
 export default function HomePage() {
+  const [value, setValue] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const BackSpace = () => {
+    try {
+      setValue(value.substring(0, value.length - 1))
+    } catch (error) {
+      console.log('Realize alguma operação')
+    }
+  }
+
+  const CalcResul = () => {
+    console.log('resultado');
+    setResult(eval(value))
+  }
+
+  const OperatorValues = (e) => {
+    if (e == "C") {
+      setValue(0)
+    } else if (value == 0) {
+      setValue(e)
+    }else if(e=="÷"){
+      setValue(value+'/')
+    }else if(e=="×"){
+      setValue(value+'*')
+    }
+    else {
+      console.log(value.length);
+      setValue(value + e)
+    }
+  }
+
   return (
     <Container>
-      <Screen/>
-        <Delete/>
-        <Buttons/>
+      <Screen value={value} result={result} />
+      <Delete BackSpace={BackSpace} />
+      <Buttons OperatorValues={OperatorValues} CalcResul={CalcResul} />
     </Container>
   )
 }
